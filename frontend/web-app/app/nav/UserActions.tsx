d55910f570b50ecab566b01d7463c0a04cf2aeec
Carlos Porras <carlosporrasc26@gmail.com>
2023-12-26 12:00:40 -0600

    Remove dev session and fix ID in bid-svc

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -41,11 +41,7 @@ export default function UserActions({ user }: Props) {
         <Dropdown.Item icon={AiFillCar}>
           <Link href="/auctions/create">Sell my car</Link>
         </Dropdown.Item>
-
-        <Dropdown.Item icon={HiCog}>
-          <Link href="/session">Session (dev only)</Link>
-        </Dropdown.Item>
-
+        
         <Dropdown.Divider />
 
         <Dropdown.Item
